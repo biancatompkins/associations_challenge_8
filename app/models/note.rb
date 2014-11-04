f417d3417728ab75e3280ff45004d89d99fbbec8
biancatompkins <biancatompkins@gmail.com>
2014-11-04 12:06:28 -0500

    complete

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ActiveRecord::Base
   belongs_to :order
-  belongs_to :customer, :through => :order
+  belongs_to :customer
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
-  has_one :note
+  has_many :notes
   
   validate :number, presence: true
   validates :number, uniqueness: true
@@ -8,7 +8,7 @@ class Order < ActiveRecord::Base
   before_create :generate_number
 
   private
-  
+
   def generate_number
     self.number = "R"+rand(999999999).to_s.center(9, rand(9).to_s)
   end
